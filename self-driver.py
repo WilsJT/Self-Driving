31ea43457672aebaf71c6d192bd3c76ff7c252f3
Wilson Tan <69861524+WilsJT@users.noreply.github.com>
2022-05-04 23:37:10 -0700

    Updated self-driver.py
    Updated region of interest and edge detection

--- self-driver.py
+++ self-driver.py
@@ -87,112 +87,6 @@ def closest_dist(pixels):
 
     return(dist, np.array(points))
 
-# def closest_dist(mask):
-#     points = np.argwhere(mask > 200)
-#
-#     pos = []
-#     # 30 Degrees
-#     for point in points:
-#         m = np.tan(-np.pi/6)
-#         pred = int(m*point[1])+1092
-#         if (point[0] == pred or point[0] + 1 == pred or point[0] - 1 == pred):
-#             pos.append(point)
-#
-#     # 45 Degrees
-#     for point in points:
-#         m = np.tan(-np.pi/4)
-#         pred = int(m*point[1])+1092
-#         if (point[0] == pred or point[0] + 1 == pred or point[0] - 1 == pred):
-#             pos.append(point)
-#
-#     # 60 Degrees
-#     for point in points:
-#         m = np.tan(-np.pi/3)
-#         pred = int(m*point[1])+1092
-#         if (point[0] == pred or point[0] + 1 == pred or point[0] - 1 == pred):
-#             pos.append(point)
-#
-#     # 120 Degrees
-#     for point in points:
-#         m = np.tan(np.pi/3)
-#         pred = int(m*point[1])+1092
-#         if (point[0] == pred or point[0] + 1 == pred or point[0] - 1 == pred):
-#             pos.append(point)
-#
-#     # 1
-#     for point in points:
-#         m = np.tan(np.pi/4)
-#         pred = int(m*point[1])+1092
-#         if (point[0] == pred or point[0] + 1 == pred or point[0] - 1 == pred):
-#             pos.append(point)
-#
-#     for point in points:
-#         m = np.tan(np.pi/6)
-#         pred = int(m*point[1])+1092
-#         if (point[0] == pred or point[0] + 1 == pred or point[0] - 1 == pred):
-#             pos.append(point)
-
-
-
-
-# edges = cv2.Canny(image, 100, 200)
-# # mask = np.zeros_like(edges)
-# # cv2.line(mask, (410, 382), (0, -328), (255,255,255), 1)
-# # cv2.line(mask, (410, 382), (410, 0), (255,255,255), 1)
-# # cv2.line(mask, (410, 382), (820, -328), (255,255,255), 1)
-#
-# # masked = cv2.bitwise_and(edges, mask, 255)
-# print(image.shape)
-# print("BITWISE_AND")
-# start = timeit.default_timer()
-# mask = cv2.bitwise_and(edges, lines)
-# end = timeit.default_timer()-start
-# print(end)
-# points = np.argwhere(mask > 200)
-# print(points)
-# # ys, xs = np.where(mask!=0)
-# # a = [i for i in zip(xs, ys)]
-# # # print(x, y)
-# # # cv2.imwrite("lines.jpg", mask)
-# # # print(xs, ys)
-# #
-# # print(a)
-# print("FOR LOOPS")
-# start = timeit.default_timer()
-# pos = []
-# for point in points:
-#     m = np.tan(-np.pi/3)
-#     pred = int(m*point[1])+1092
-#     # print(point[0], pred)
-#     if (point[0] == pred or point[0] + 1 == pred or point[0] - 1 == pred):
-#         pos.append(point)
-#
-# for point in points:
-#     m = np.tan(np.pi/3)
-#     pred = int(m*(point[1]))-328
-#     # print(point[0], pred)
-#     if (point[0] == pred or point[0] + 1 == pred or point[0] - 1 == pred):
-#         pos.append(point)
-# end = timeit.default_timer()-start
-# print(end)
-#
-# print(pos)
-# curr = [100000, None]
-# for point in pos:
-#     dist = np.linalg.norm(point - np.array([382, 410]))
-#     if (dist < curr[0]):
-#         curr = [dist, point]
-#
-# print(curr)
-#     # -point[0], m*point[1]+328)
-#
-#
-#
-# cv2.circle(image, (410, 82), 5, (0,255,0), -1)
-# cv2.circle(image, (263, 128), 5, (255,0,0), -1)
-# cv2.circle(image, (557, 128), 5, (0,0,255), -1)
-# print(points)
-
 while True:
     # Grab screen for lanes
     grab = ImageGrab.grab(bbox=(0, 27, 1020, 565))
@@ -201,14 +95,14 @@ while True:
     # Grab portion of the screen for speed
     grab = ImageGrab.grab(bbox=(828, 56, 904, 100))
 
-    # Get speed with tesseract
-    speed = pytesseract.image_to_string(grab, config="--psm 6")
+    # # Get speed with tesseract
+    # speed = pytesseract.image_to_string(grab, config="--psm 6")
 
     # Convert color to gray
     image = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
 
     # Define a region of interest
-    dim = np.array([[0, 300], [0, 570], [1020, 570], [1020, 300], [510, 220], [410, 220]])
+    dim = np.array([[0, 275], [0, 570], [1020, 570], [1020, 275], [560, 260], [460, 260]])
     masked = roi(image, [dim])
 
     # Detect edges
@@ -217,7 +111,7 @@ while True:
 
     # Get lanes
     lane = np.zeros_like(edges)
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 180, np.array([]), 225, 10)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 180, np.array([]), 150, 4)
     draw_lines(lane, lines)
 
     # Get pixels
@@ -231,7 +125,6 @@ while True:
     for point in points:
         cv2.line(screen, (510, 538), (point[1], point[0]), (0,255,0), 1)
         cv2.circle(screen, (point[1], point[0]), 5, (0,0,255), -1)
-
 
     cv2.imshow("lane", lane)
     cv2.imshow("image", screen)
